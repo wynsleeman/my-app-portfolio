978965112f04922a3533ae39806b8aa16496a8ef
root <root@ip-10-47-174-141.ec2.internal>
2015-06-08 21:39:20 +0000

    Committed by Udacity

---
+++ app/src/main/java/sleemanuk/nanodegreeprojectzero/MainActivity.java
@@ -0,0 +1,72 @@
+package sleemanuk.nanodegreeprojectzero;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    Toast toast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    public void launchApp(View view)
+    {
+        Button b = (Button) view;
+        String s = String.format(getResources().getString(R.string.This_Will_Launch_App_When_Built), b.getText());
+
+        showToast(s);
+    }
+
+    private void showToast(String message)
+    {
+        if(toast != null) toast.cancel();
+        toast=Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+
+
+    public void cannotLaunchApp(View view)
+    {
+        Button b = (Button) view;
+        String s = String.format(getResources().getString(R.string.App_Is_Not_Built_Yet), b.getText());
+        showToast(s);
+
+    }
+}
